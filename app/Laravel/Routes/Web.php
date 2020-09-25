0a33620efe9764bf758f01d443a18723e776ef81
ronniecastro <ronnie.castro54@gmail.com>
2020-09-25 08:16:52 +0800

    update

--- app/Laravel/Routes/Web.php
+++ app/Laravel/Routes/Web.php
@@ -47,7 +47,7 @@ Route::group(['as' => "web.",
 		
 	});
 
-	Route::any('confirmation/{code}',['as' => "confirmation",'uses' => "MainController@confirmation"]);
+	Route::get('confirmation/{code}',['as' => "confirmation",'uses' => "MainController@confirmation"]);
 	
 
 });
